eb120f108fef19abb235915e4dbb40680f1f6360
M4rFri <marcel.fritz96@gmail.com>
2024-07-05 12:57:01 +0200

    Updated log_msg to accept attributes

--- src/details/log_msg.cpp
+++ src/details/log_msg.cpp
@@ -12,6 +12,25 @@ log_msg::log_msg(spdlog::log_clock::time_point log_time,
                  spdlog::source_loc loc,
                  string_view_t a_logger_name,
                  spdlog::level lvl,
+                 spdlog::string_view_t msg,
+                 spdlog::log_attributes attributes)
+    : logger_name(a_logger_name),
+      log_level(lvl),
+      time(log_time)
+#ifndef SPDLOG_NO_THREAD_ID
+      ,
+      thread_id(os::thread_id())
+#endif
+      ,
+      source(loc),
+      payload(msg),
+      attributes(attributes) {
+}
+
+log_msg::log_msg(spdlog::log_clock::time_point log_time,
+                 spdlog::source_loc loc,
+                 string_view_t a_logger_name,
+                 spdlog::level lvl,
                  spdlog::string_view_t msg)
     : logger_name(a_logger_name),
       log_level(lvl),
@@ -28,8 +47,18 @@ log_msg::log_msg(spdlog::log_clock::time_point log_time,
 log_msg::log_msg(spdlog::source_loc loc, string_view_t a_logger_name, spdlog::level lvl, spdlog::string_view_t msg)
     : log_msg(os::now(), loc, a_logger_name, lvl, msg) {}
 
+log_msg::log_msg(spdlog::source_loc loc,
+                 string_view_t a_logger_name,
+                 spdlog::level lvl,
+                 spdlog::string_view_t msg,
+                 spdlog::log_attributes attributes)
+    : log_msg(os::now(), loc, a_logger_name, lvl, msg, attributes) {}
+
 log_msg::log_msg(string_view_t a_logger_name, spdlog::level lvl, spdlog::string_view_t msg)
     : log_msg(os::now(), source_loc{}, a_logger_name, lvl, msg) {}
+
+log_msg::log_msg(string_view_t a_logger_name, spdlog::level lvl, spdlog::string_view_t msg, spdlog::log_attributes attributes)
+    : log_msg(os::now(), source_loc{}, a_logger_name, lvl, msg, attributes) {}
 
 }  // namespace details
 }  // namespace spdlog
